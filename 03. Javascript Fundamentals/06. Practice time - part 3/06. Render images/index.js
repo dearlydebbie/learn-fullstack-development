9e8a9c63b24e1107ca7571bd3456ba7224c1fced
Deborah Adeoye <debbieadeoye1@gmail.com>
2025-10-20 14:19:15 +0100

    js practice

--- 03. Javascript Fundamentals/06. Practice time - part 3/06. Render images/index.js
+++ 03. Javascript Fundamentals/06. Practice time - part 3/06. Render images/index.js
@@ -7,3 +7,12 @@ const imgs = [
     "images/hip2.jpg",
     "images/hip3.jpg"
 ]
+function renderImages(imageArray) {
+    const container = document.getElementById("imageContainer")
+    let imgHTML = ""
+    for (let i = 0; i < imageArray.length; i++) {
+        imgHTML += `<img src="${imageArray[i]}" alt="Hipster Image ${i + 1}">`
+    }
+    container.innerHTML = imgHTML
+}
+renderImages(imgs)
